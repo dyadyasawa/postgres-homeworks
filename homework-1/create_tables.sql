8b6f8d5289de97d6cc256c7d4289f8f45f499057
dyadyasawa <dyadyasawa@gmail.com>
2024-02-01 11:08:48 +0300

    Исправления(запятые) в create_tables.sql.

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,26 +1,26 @@
 -- SQL-команды для создания таблиц
 CREATE TABLE employees
 (
-	employee_id int PRIMARY KEY
-	first_name varchar(100) NOT NULL
-	last_name varchar(100) NOT NULL
-	title varchar(100) NOT NULL
-	birth_data date NOT NULL
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_data date NOT NULL,
 	notes text
 );
 
 CREATE TABLE customers
 (
-	customer_id varchar(5) PRIMARY KEY
-	company_name varchar(100) NOT NULL
+	customer_id varchar(5) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
 	contact_name varchar(100) NOT NULL
 );
 
 CREATE TABLE orders
 (
-	order_id int PRIMARY KEY
-	client varchar(5) REFERENCES customers(customer_id) NOT NULL
-	executor int REFERENCES employees(employee_id) NOT NULL
-	order_date date NOT NULL
+	order_id int PRIMARY KEY,
+	client varchar(5) REFERENCES customers(customer_id) NOT NULL,
+	executor int REFERENCES employees(employee_id) NOT NULL,
+	order_date date NOT NULL,
 	ship_city varchar(100) NOT NULL
 );
