6b1764400d7738d797fd9405e9bf36ebf309db5e
dyadyasawa <dyadyasawa@gmail.com>
2024-02-01 14:31:30 +0300

    Импорт необходимых библиотек. Реализация подключения к базе данных и заполнения таблиц.

--- homework-1/main.py
+++ homework-1/main.py
@@ -28,3 +28,36 @@ postgres=# \l
 (4 rows)
 
 """
+
+import os
+import psycopg2
+from csv import DictReader
+
+password = os.getenv('PASSWORD_FOR_DB') # Пароль для базы данных. Находится в переменной окружения.
+
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password=password)
+try:
+    with conn:
+
+        with conn.cursor() as curs:
+
+            with open('employees_data.csv', newline='', encoding='utf-8') as f:
+                reader = DictReader(f)
+                for row in reader:
+                    result_tuple = (int(row['employee_id']), row['first_name'], row['last_name'], row['title'], string_to_date(row['birth_date']), row['notes'])
+                    curs.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', result_tuple)
+
+            with open('customers_data.csv', newline='', encoding='utf-8') as f:
+                reader = DictReader(f)
+                for row in reader:
+                    result_tuple = (row['customer_id'], row['company_name'], row['contact_name'])
+                    curs.execute('INSERT INTO customers VALUES (%s, %s, %s)', result_tuple)
+
+            with open('orders_data.csv', newline='', encoding='utf-8') as f:
+                reader = DictReader(f)
+                for row in reader:
+                    result_tuple = (int(row['order_id']), row['customer_id'], int(row['employee_id']), string_to_date(row['order_date']), row['ship_city'])
+                    curs.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', result_tuple)
+
+finally:
+    conn.close()
