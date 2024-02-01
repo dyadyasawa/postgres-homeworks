65edb49cb56f7013bb0ac0aeb07c6974eac94822
dyadyasawa <dyadyasawa@gmail.com>
2024-02-01 08:16:50 +0300

    Добавление в create_tables.sql.

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY
+	first_name varchar(100) NOTNULL
+	last_name varchar(100) NOTNULL
+	title varchar(100) NOTNULL
+	birth_data date
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(5) PRIMARY KEY
+	company_name varchar(100) NOTNULL
+	contact_name varchar(100) NOTNULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY
+	customer_id varchar(5) NOTNULL
+	employee_id int NOTNULL
+	order_date date NOTNULL
+	ship_city varchar(100) NOTNULL
+);
