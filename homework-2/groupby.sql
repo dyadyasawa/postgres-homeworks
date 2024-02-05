e9c6f2efb669701ba96d6e54486f3a036eb3115e
dyadyasawa <dyadyasawa@gmail.com>
2024-02-05 08:53:29 +0300

    Дополнение в groupby.sql.

--- homework-2/groupby.sql
+++ homework-2/groupby.sql
@@ -15,6 +15,7 @@ SELECT country, COUNT(*) FROM suppliers GROUP BY country ORDER BY COUNT(*) DESC;
 SELECT ship_country, SUM(freight) FROM orders WHERE ship_region IS NOT NULL GROUP BY ship_country HAVING SUM(freight) > 2750 ORDER BY SUM(freight) DESC;
 
 -- 6. страны, в которых зарегистрированы и заказчики (customers) и поставщики (suppliers) и работники (employees).
-
+SELECT country FROM customers INTERSECT SELECT country FROM suppliers INTERSECT SELECT country FROM employees;
 
 -- 7. страны, в которых зарегистрированы и заказчики (customers) и поставщики (suppliers), но не зарегистрированы работники (employees).
+SELECT country FROM customers INTERSECT SELECT country FROM suppliers EXCEPT SELECT country FROM employees;
